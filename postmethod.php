f9b18b2acf0e71f11a95ed1bbaef9c956ab09f19
devaryan42 <devaryan42@gmail.com>
2018-04-06 09:12:42 +0530

    code updated

--- postmethod.php
+++ postmethod.php
@@ -2,7 +2,7 @@
 
 ?>
 <form action="result.php" method="post">
-<input type="file" id="file" name="file">
+<input type="file" id="file1" name="file">
 <input type="submit">
 
 </form>
--- result.php
+++ result.php
@@ -1,5 +1,4 @@
 <?php
 
-if (isset($_POST['submit'])){
-    echo $_POST['file'];
-}
+
+echo $_POST['file'];
